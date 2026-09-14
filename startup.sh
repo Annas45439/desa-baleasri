a6278ecb44e3beb81647fa39686aa3b4f2776bf5
Annas Ridho Sadila <25051204368@mhs.unesa.ac.id>
2026-09-15 02:17:20 +0700

    Copy public assets for Azure web root

--- startup.sh
+++ startup.sh
@@ -10,6 +10,7 @@ if [ ! -f index.php ]; then
 	printf '%s\n' '<?php' "require __DIR__ . '/public/index.php';" > index.php
 fi
 
-ln -sfn public/assets assets
-ln -sfn public/build build
+rm -rf assets build
+cp -R public/assets assets
+cp -R public/build build
 php artisan storage:link --force || true
