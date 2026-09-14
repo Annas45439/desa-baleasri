4bcd892458dd2572012d2be08de4a5f8acff1423
Annas Ridho Sadila <25051204368@mhs.unesa.ac.id>
2026-09-15 01:12:40 +0700

    Serve Laravel public entrypoint on Azure

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
 
+cd /home/site/wwwroot
+rm -f hostingstart.html
+
+if [ ! -f index.php ]; then
+	printf '%s\n' '<?php' "require __DIR__ . '/public/index.php';" > index.php
+fi
+
+ln -sfn public/assets assets
+ln -sfn public/build build
 php artisan storage:link --force || true
