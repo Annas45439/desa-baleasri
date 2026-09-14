e3ffb63e895ad6c20acfcc2ef2cca4fadc32cd63
Annas Ridho Sadila <25051204368@mhs.unesa.ac.id>
2026-09-15 02:16:01 +0700

    Remove Azure HTTPS redirect loop

--- app/Http/Middleware/ProductionSecurityMiddleware.php
+++ app/Http/Middleware/ProductionSecurityMiddleware.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class ProductionSecurityMiddleware
 {
@@ -14,20 +13,6 @@ class ProductionSecurityMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        $request->setTrustedProxies(['*'],
-            SymfonyRequest::HEADER_X_FORWARDED_FOR
-            | SymfonyRequest::HEADER_X_FORWARDED_HOST
-            | SymfonyRequest::HEADER_X_FORWARDED_PORT
-            | SymfonyRequest::HEADER_X_FORWARDED_PROTO
-            | SymfonyRequest::HEADER_X_FORWARDED_PREFIX
-        );
-
-        $isHttps = $request->secure() || $request->header('X-Forwarded-Proto') === 'https';
-
-        if (app()->environment('production') && ! $isHttps) {
-            return redirect()->secure($request->getRequestUri());
-        }
-
         $response = $next($request);
 
         $response->headers->set('X-Content-Type-Options', 'nosniff');
